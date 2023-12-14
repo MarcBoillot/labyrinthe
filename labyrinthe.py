70b400b10668c818f161b10516cde6e7fa08b790
MarcBoillot <marc.boillot@le-campus-numerique.fr>
2023-12-14 16:03:53 +0100

    py

--- labyrinthe.py
+++ labyrinthe.py
@@ -1,43 +1,51 @@
-labyrinth =
-    (S,■,0,0,0,0,0),
-    (0,■,0,■,■,0,■),
-    (0,■,0,■,G,0,0),
-    (0,0,0,0,■,■,0),
-    (0,■,0,■,0,0,0),
-    (0,■,0,0,0,■,0),
+labyrinth = [
+    ("S","|",".",".",".",".","."),
+    (".","|",".","|","|",".","|"),
+    (".","|",".","|","G",".","."),
+    (".",".",".",".","|","|","."),
+    (".","|",".","|",".",".","."),
+    (".","|",".",".",".","|",".")
+    ]
 count_step=0
-G = (4,2)
-player_position = (0,0)
+G=(4,2)
 S = (0,0)
-def check_case_around(x,y):
+player_position = S
+def check_case_around(position):
     next_case=[]
+    x,y=position
     for dx,dy in [(1,0),(-1,0),(0,1),(0,-1)]:
-        new_x = (x+dx)
-        new_y = (y+dy)
-        if new_x in next_case && new_y in next_case:
-            next_case = (new_x,new_y)
+        new_x = x+dx
+        new_y = y+dy
+        if 0 <= new_x < len(labyrinth) and 0 <= new_y < len(labyrinth[0]) and labyrinth[new_x][new_y] != "|":
+            next_case.append((new_x, new_y))
     return next_case
 
-def move (player_position):
-    player_position = check_case_around(player_position)
+def move (player_position, next_position):
+
+    player_position = next_position
+    return next_position
+
+def case(next_position):
+    return labyrinth[next_position[0]][next_position[1]]
 
 
-def solution_labyrinth ():
-    global player_position, count_step
+def solution_labyrinth (count_step, tab):
+
     while player_position != G:
-        while check_case_around(player_position)=0:
-            if (case(check_case_around(player_position))=1):
-                case(check_case_around(player_position))+=1
+        check_case_around(player_position)
+        while check_case_around(player_position)==0:
+            if case(check_case_around(player_position))==1:
+                case[player_position]+=1
                 move(player_position)
                 count_step+=1
-        while (case(check_case_around(player_position))=1):
+        while case(check_case_around(player_position))==1:
             move(player_position)
             count_step+=1
             check_case_around(player_position)
-            if case(check_case_around(player_position))=0):
-                if tab.length = 2:
-                    case(check_case_around(player_position))+=1
+            if case(check_case_around(player_position))==0:
+                if tab.length == 2:
+                    case[player_position]+=1
                     move(player_position,check_case_around(player_position),tab)
-                    count_step++
-    print("total steps, count_step")
-solution_labyrinth()
+            count_step+=1
+print("Total steps:", count_step)
+solution_labyrinth(count_step,labyrinth)
